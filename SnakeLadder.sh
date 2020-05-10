e0860cb51d6419296d4645b881c618058e5bbeb2
Nikita <shriramnikita@gmail.com>
2020-05-10 21:08:30 +0530

    Added: player gets to exactly winning pos 100

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -5,34 +5,40 @@ echo "********Snake And Ladder*******"
 #uc1 add player and intialize position of the player 
 position=0
 
+endPosition=100
+
+
 #uc4 play unitl winning position
 while [ $position -le 100 ]
 do
 
-#uc2 roll die toget number between 1 to 6
-rollDie=$(($RANDOM%6 + 1 ))
+	#uc2 roll die toget number between 1 to 6
+	rollDie=$(($RANDOM%6 + 1 ))
 
 
-#uc3 checks for position where if pos=0 no move,pos=1 ladder move ahead by rolldie, pos=2 snake go down by rolldie
-option=$(($RANDOM%3))
+	#uc3 checks for position where if pos=0 no move,pos=1 ladder move ahead by rolldie, pos=2 snake go down by rolldie
+	option=$(($RANDOM%3))
 
-case $option in
+	case $option in
 
-	0)
-		position=$position
-	;;
-	1)
-		position=$(($position + $rollDie))
-	;;
-	2)
-		position=$(($position - $rollDie))
-	;;
-esac
+		0)
+			position=$position
+		;;
+		1)
+			position=$(($position + $rollDie))
+		;;
+		2)
+			position=$(($position - $rollDie))
+		;;
+	esac
 
-if [ $position -lt 0 ]
+				if [ $position -lt 0 ]
 				then
 					position=0
+				elif [ $position -gt $endPosition ]
+				then
+					position=$(($postition-$diceRandom))
 				else
-					echo "position:- "$position
+					echo "Position--> " $position
 				fi
 done

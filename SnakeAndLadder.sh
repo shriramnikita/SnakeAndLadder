1540fa4362c2161e3ec7ab3f31a05a23b7cfd39e
Nikita <shriramnikita@gmail.com>
2020-05-10 16:56:23 +0530

    Added:repeats till player reaches win position 100

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -3,21 +3,27 @@
 echo "********Snake And Ladder*******"
 
 #uc1 add player and intialize position of the player 
-position=0
-read -p "Enter first player name:- " player1
-read -p "Enter second player name:- " player2
+endPosition=100
+diceCount=0
+player1Position=0
+player2Position=0
+
+#read -p "Enter first player name:- " player1
+#read -p "Enter second player name:- " player2
 
 
-#uc2 roll die toget number between 1 to 6
+function getPosition()
+{
+	position=$1
+	#uc2 roll die toget number between 1 to 6	
+	rollDie=$(($RANDOM%6 + 1 ))
+	
+	((diceCount++))
 
-rollDie=$(($RANDOM%6 + 1 ))
+	#uc3 checks for position where if pos=0 no move,pos=1 ladder move ahead by rolldie, pos=2 snake go down by rolldie
+	option=$(($RANDOM%3))
 
-echo  $rollDie
-
-#uc3 checks for position where if pos=0 no move,pos=1 ladder move ahead by rolldie, pos=2 snake go down by rolldie
-option=$(($RANDOM%3))
-
-case $option in
+	case $option in
 
 	0)
 		position=0
@@ -28,8 +34,41 @@ case $option in
 	2)
 		position=$(($position - $rollDie))
 	;;
-esac
+	esac
+				if [ $position -lt 0 ]
+				then
+					position=0
+				elif [ $position -gt $endPosition ]
+				then
+					position=$(($postition-$diceRandom))
+				fi
+					echo "$position"
+}
 
-echo $rollDie
-echo $option
-echo $position
+#func call
+getPosition
+
+
+read -p "Enter first player name:- " player1
+read -p "Enter second player name:- " player2
+
+
+#uc4 repeat till player reaches winning pos
+while [[ $player1Position -lt $endPosition && $player2Position -lt $endPosition ]]
+do
+
+player1Position=$(getPosition $player1Position)
+player2Position=$(getPosition $player2Position)
+
+echo "player 1 position :-" $player1Position
+echo "player 2 position :-" $player2Position
+
+	if [[ $player1Position -gt  $player2Position && $player1Position -eq $endPosition ]]
+	then
+		echo "Congratulations, $player1 you WIN!!!"
+	elif [ $player2Position -eq $endPosition ] 
+	then
+		echo "Congratulations, $player2 you WIN!!!"
+	fi
+
+done

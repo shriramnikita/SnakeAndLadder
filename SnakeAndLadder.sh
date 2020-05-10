89ca9e97710c08457de78bc8abc4faf795a10304
Nikita <shriramnikita@gmail.com>
2020-05-10 16:18:02 +0530

    Added: Welcome message added

---
+++ SnakeAndLadder.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "********Snake And Ladder*******"
--- SnakeLadder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "********Snake And Ladder*******
-
-#uc1 add player and intialize position of the player 
-position=0
-
-read -p "Enter first player name:- " player1
-read -p "Enter second player name:- " player2

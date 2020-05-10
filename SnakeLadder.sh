6ea7fd93127c7754d2a6dbdc695bc6a8b13371dd
Nikita <shriramnikita@gmail.com>
2020-05-10 20:17:00 +0530

    Added:repeats till player reaches win position 100

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -5,11 +5,13 @@ echo "********Snake And Ladder*******"
 #uc1 add player and intialize position of the player 
 position=0
 
-#uc2 roll die toget number between 1 to 6
+#uc4 play unitl winning position
+while [ $position -le 100 ]
+do
 
+#uc2 roll die toget number between 1 to 6
 rollDie=$(($RANDOM%6 + 1 ))
 
-echo  $rollDie
 
 #uc3 checks for position where if pos=0 no move,pos=1 ladder move ahead by rolldie, pos=2 snake go down by rolldie
 option=$(($RANDOM%3))
@@ -17,7 +19,7 @@ option=$(($RANDOM%3))
 case $option in
 
 	0)
-		position=0
+		position=$position
 	;;
 	1)
 		position=$(($position + $rollDie))
@@ -27,6 +29,10 @@ case $option in
 	;;
 esac
 
-echo $rollDie
-echo $option
-echo $position
+if [ $position -lt 0 ]
+				then
+					position=0
+				else
+					echo "position:- "$position
+				fi
+done

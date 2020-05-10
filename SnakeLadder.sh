e03cfd5e17026d7088d449172c1889511a18af86
Nikita <shriramnikita@gmail.com>
2020-05-10 20:08:38 +0530

    Added:checks option if snake , ladder or no move

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -10,3 +10,23 @@ position=0
 rollDie=$(($RANDOM%6 + 1 ))
 
 echo  $rollDie
+
+#uc3 checks for position where if pos=0 no move,pos=1 ladder move ahead by rolldie, pos=2 snake go down by rolldie
+option=$(($RANDOM%3))
+
+case $option in
+
+	0)
+		position=0
+	;;
+	1)
+		position=$(($position + $rollDie))
+	;;
+	2)
+		position=$(($position - $rollDie))
+	;;
+esac
+
+echo $rollDie
+echo $option
+echo $position

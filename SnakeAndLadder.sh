1b2c301a300ce680dad37f5a428060d5efd7418b
Nikita <shriramnikita@gmail.com>
2020-05-10 16:34:47 +0530

    Added: roll die to get no. between 1 to 6

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -6,3 +6,10 @@ echo "********Snake And Ladder*******"
 position=0
 read -p "Enter first player name:- " player1
 read -p "Enter second player name:- " player2
+
+
+#uc2 roll die toget number between 1 to 6
+
+rollDie=$(($RANDOM%6 + 1 ))
+
+echo  $rollDie

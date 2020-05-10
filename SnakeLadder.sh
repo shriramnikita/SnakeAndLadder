da824cfbd2d6f56ee6ca39d0f81d3350208ec834
Nikita <shriramnikita@gmail.com>
2020-05-10 15:29:56 +0530

    Edited: added players

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -2,5 +2,8 @@
 
 echo "********Snake And Ladder*******
 
-#uc1 intialize position for player
+#uc1 add player and intialize position of the player 
 position=0
+
+read -p "Enter first player name:- " player1
+read -p "Enter second player name:- " player2
